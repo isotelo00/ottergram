7ee94769166dc789a8d9427448cd497bb6bff4b7
isotelo00 <immanuelsotelo00@gmail.com>
2021-09-17 14:52:38 -0700

    beautified code

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,11 @@ var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
 var TINY_EFFECT_CLASS = 'is-tiny';
-var images = ['img/otter1.jpg','img/otter2.jpg','img/otter3.jpg','img/otter4.jpg','img/otter5.jpg']
-var titles = ['Stayin\' Alive', 'How Deep is Your Love','You Should Be Dancing','Night Fever','To Love Somebody']
+var images = ['img/otter1.jpg', 'img/otter2.jpg', 'img/otter3.jpg', 'img/otter4.jpg', 'img/otter5.jpg']
+var titles = ['Stayin\' Alive', 'How Deep is Your Love', 'You Should Be Dancing', 'Night Fever', 'To Love Somebody']
 var ESC_KEY = 27;
 var index = 0;
+
 function setDetails(imageUrl, titleText) {
     'use strict';
     var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -15,17 +16,21 @@ function setDetails(imageUrl, titleText) {
     var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
     detailTitle.textContent = titleText;
 }
+
 function imageFromThumb(thumb) {
     'use strict';
     return thumb.getAttribute('data-image-url');
 }
+
 function titleFromThumb(thumb) {
     'use strict';
     return thumb.getAttribute('data-image-title');
 }
+
 function setDetailsFromThumb(thumb) {
     setDetails(imageFromThumb(thumb), titleFromThumb(thumb));
 }
+
 function addThumbClickHandler(thumb) {
     'use strict';
     thumb.addEventListener('click', function (event) {
@@ -35,17 +40,19 @@ function addThumbClickHandler(thumb) {
         showDetails();
     });
 }
-function prev(){
-    if(index <= 0) index = images.length;
+
+function prev() {
+    if (index <= 0) index = images.length;
     index--;
     console.log(index);
-    return  setDetails(images[index], titles[index]);
+    return setDetails(images[index], titles[index]);
 }
-function next(){
-    if(index >= images.length-1) index = -1;
+
+function next() {
+    if (index >= images.length - 1) index = -1;
     index++;
     console.log(index);
-    return  setDetails(images[index], titles[index]);
+    return setDetails(images[index], titles[index]);
 }
 
 function getThumbnailsArray() {
@@ -54,10 +61,12 @@ function getThumbnailsArray() {
     var thumbnailArray = [].slice.call(thumbnails);
     return thumbnailArray;
 }
+
 function hideDetails() {
     'use strict';
     document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
+
 function showDetails() {
     'use strict';
     var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
@@ -67,6 +76,7 @@ function showDetails() {
         frame.classList.remove(TINY_EFFECT_CLASS);
     }, 50);
 }
+
 function addKeyPressHandler() {
     'use strict';
     document.body.addEventListener('keyup', function (event) {
@@ -77,6 +87,7 @@ function addKeyPressHandler() {
         }
     });
 }
+
 function initializeEvents() {
     'use strict';
     var thumbnails = getThumbnailsArray();
